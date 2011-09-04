373964561b8c3127bd400ad3d5702140cf618c66
sugyan <sugi1982@gmail.com>
2011-09-04 21:01:09 +0900

    realtime comment

--- public/javascripts/momoclo.js
+++ public/javascripts/momoclo.js
@@ -4,6 +4,7 @@ var onFinishAddCallback = function () {
         url: '/api/program',
         dataType: 'json',
         success: function (data) {
+            $('#info').text(data.title);
             player.sync(data);
         }
     });
@@ -14,4 +15,35 @@ $(function () {
         '/swf/player.swf',
         'player','480', '360', "10.0.0", null, {}, {}, {}
     );
+
+    var prependMessage = function (data) {
+        $('#messages').prepend(
+            $('<div>').addClass('message')
+                .append($('<span>').addClass('name').text(data.name))
+                .append($('<span>').addClass('text').text(data.text))
+        );
+        while ($('.message').length > 100) {
+            $('.message').last().remove();
+        }
+    };
+
+    var socket = io.connect();
+    socket.on('comment', prependMessage);
+
+    var input = $('#message');
+    $('#comments').submit(function (e) {
+        e.preventDefault();
+        var text = input.val();
+        if (text.length > 0) {
+            if (text.length < 50) {
+                socket.emit('comment', input.val());
+                prependMessage({
+                    name: 'you',
+                    text: text
+                });
+            }
+            input.val('');
+        }
+    });
+    input.focus();
 });
