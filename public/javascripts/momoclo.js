b6f0d34b83528088f0bdba0900ef05dcb09c8c32
sugyan <sugi1982@gmail.com>
2011-09-04 21:22:34 +0900

    コメント時間と最近10件の保存

--- public/javascripts/momoclo.js
+++ public/javascripts/momoclo.js
@@ -17,8 +17,15 @@ $(function () {
     );
 
     var prependMessage = function (data) {
+        var date = new Date(data.date);
+        var dateStr = [
+            date.getHours()   < 10 ? '0' + date.getHours()   : String(date.getHours()),
+            date.getMinutes() < 10 ? '0' + date.getMinutes() : String(date.getMinutes()),
+            date.getSeconds() < 10 ? '0' + date.getSeconds() : String(date.getSeconds())
+        ].join(':');
         $('#messages').prepend(
             $('<div>').addClass('message')
+                .append($('<span>').addClass('date').text(dateStr))
                 .append($('<span>').addClass('name').text(data.name))
                 .append($('<span>').addClass('text').text(data.text))
         );
@@ -38,6 +45,7 @@ $(function () {
             if (text.length < 50) {
                 socket.emit('comment', input.val());
                 prependMessage({
+                    date: new Date().getTime(),
                     name: 'you',
                     text: text
                 });
