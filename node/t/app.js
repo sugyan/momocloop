198f436e8c01f976b439a464b53ea62798820b2f
sugyan <sugi1982@gmail.com>
2011-09-04 09:54:57 +0900

    TAP tests

--- node/t/app.js
+++ node/t/app.js
@@ -1,1 +1,16 @@
+var test = require('tap').test;
 var app = require('../lib/app');
+
+var port = 3333;                // FIXME
+
+test('app', function (t) {
+    app.on('listening', function () {
+        t.ok(true, 'start');
+        app.on('close', function () {
+            t.ok(true, 'end');
+            t.end();
+        });
+        app.close();
+    });
+    app.listen(port);
+});
--- node/t/socket.io.js
+++ node/t/socket.io.js
@@ -1,1 +1,9 @@
+var test = require('tap').test;
 var io = require('../lib/socket.io');
+
+var port = 3334;                // FIXME
+
+test('socket.io', function (t) {
+    t.ok(true);
+    t.end();
+});
