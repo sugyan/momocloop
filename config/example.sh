83a533e06a76eb36852b4cf4e19997ee6d4047c6
sugyan <sugi1982@gmail.com>
2011-09-09 01:42:35 +0900

    config

---
+++ config/example.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+npm config set momocloop:_ustream_key ********************************
+npm config set momocloop:_twitter_consumer_key **********************
+npm config set momocloop:_twitter_consumer_secret ***************************************
