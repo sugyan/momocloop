76611e363327612e75e7e6f26d4394770e2b056e
sugyan <sugi1982@gmail.com>
2011-09-11 01:53:08 +0900

    liveとtalkの分離,top page. socket.ioまわりの処理を分ける必要有り

--- public/javascripts/momoclo.js
+++ public/javascripts/momoclo.js
@@ -6,7 +6,7 @@ momoclo.loadStream = function () {
         url: '/api/program',
         dataType: 'json',
         data: {
-            type: 'live'
+            type: window.location.pathname === '/live' ? 'live' : 'talk'
         },
         success: function (data) {
             $('#duration').text(data.lengthInSecond);
@@ -25,62 +25,108 @@ momoclo.progress = function (time) {
     $('#time').text(str);
 };
 
-$(function () {
-    var myname = 'you';
-    var socket = io.connect();
-    // swf
-    swfobject.embedSWF('/swf/player.swf', 'player','480', '360', "10.0.0", null, {}, {}, {});
-    // comment
-    var prependMessage = function (data) {
-        var date = new Date(data.date);
-        var dateStr = [
-            date.getHours()   < 10 ? '0' + date.getHours()   : String(date.getHours()),
-            date.getMinutes() < 10 ? '0' + date.getMinutes() : String(date.getMinutes()),
-            date.getSeconds() < 10 ? '0' + date.getSeconds() : String(date.getSeconds())
-        ].join(':');
-        $('#messages').prepend(
-            $('<div>').addClass('message')
-                .append($('<span>').addClass('date').text(dateStr))
-                .append($('<span>').addClass('name').text(data.name))
-                .append($('<span>').addClass('text').text(data.text))
-        );
-        while ($('.message').length > 100) {
-            $('.message').last().remove();
-        }
-        // anywhere
-        twttr.anywhere(function (T) {
-            T('#messages').linkifyUsers({ className: 'blank' });
-            $('#messages a.blank').attr({ target: '_blank' });
-        });
-    };
-    // socket.io
-    socket.on('name', function (name) {
-        myname = name;
-    });
-    socket.on('comment', prependMessage);
-    socket.on('connection', function (data) {
-        $('#connection').text(data + '人');
-    });
-    $('#comments').submit(function (e) {
-        e.preventDefault();
-        var input = $('#message');
-        var text = input.val();
-        if (text.length > 0) {
-            if (text.length < 50) {
-                socket.emit('comment', input.val());
-                prependMessage({
-                    date: new Date().getTime(),
-                    name: myname,
-                    text: text
+if (window.location.pathname === '/') {
+    $(function () {
+        var started  = {};
+        var duration = {};
+        var toMmSsString = function (seconds) {
+            var m = Math.floor(seconds / 60);
+            var s = Math.floor(seconds % 60);
+            return m + ':' + (s < 10 ? '0' + s : s);
+        };
+        var loadProgram = function () {
+            $.each(['live', 'talk'], function (i, e) {
+                $.ajax({
+                    url: '/api/program',
+                    dataType: 'json',
+                    data: { type: e },
+                    success: function (data) {
+                        console.log(JSON.stringify(data));
+                        var div = $('#' + e);
+                        started[e]  = data.started;
+                        duration[e] = data.lengthInSecond * 1000;
+                        div.find('.image').empty().append(
+                            $('<a>').attr({ href: '/' + e })
+                                .append($('<img>').attr({ src: data.image2 }))
+                        );
+                        div.find('.title').empty().append(
+                            $('<a>').attr({ href: '/' + e }).text(data.title)
+                        );
+                        div.find('.description').text(data.description);
+                        div.find('.created').text(data.createdAt);
+                        div.find('.duration').text(toMmSsString(data.lengthInSecond));
+                    }
                 });
+            });
+        };
+        loadProgram();
+        setInterval(function () {
+            var now = new Date().getTime();
+            $.each(['live', 'talk'], function (i, e) {
+                if (started[e]) {
+                    $('#' + e + ' .nowplaying').text(toMmSsString((now - started[e]) / 1000));
+                    if (now - (started[e] + duration[e]) > 0) {
+                        console.log('load!');
+                        loadProgram();
+                    }
+                }
+            });
+        }, 200);
+    });
+}
+else {
+    $(function () {
+        var socket = io.connect();
+        var myname = 'you';
+        // comment
+        var prependMessage = function (data) {
+            var date = new Date(data.date);
+            var dateStr = [
+                date.getHours()   < 10 ? '0' + date.getHours()   : String(date.getHours()),
+                date.getMinutes() < 10 ? '0' + date.getMinutes() : String(date.getMinutes()),
+                date.getSeconds() < 10 ? '0' + date.getSeconds() : String(date.getSeconds())
+            ].join(':');
+            $('#messages').prepend(
+                $('<div>').addClass('message')
+                    .append($('<span>').addClass('date').text(dateStr))
+                    .append($('<span>').addClass('name').text(data.name))
+                    .append($('<span>').addClass('text').text(data.text))
+            );
+            while ($('.message').length > 100) {
+                $('.message').last().remove();
             }
-            input.val('');
-        }
+            // anywhere
+            twttr.anywhere(function (T) {
+                T('#messages').linkifyUsers({ className: 'blank' });
+                $('#messages a.blank').attr({ target: '_blank' });
+            });
+        };
+        // swf
+        swfobject.embedSWF('/swf/player.swf', 'player','480', '360', "10.0.0", null, {}, {}, {});
+        // socket.io
+        socket.on('name', function (name) {
+            myname = name;
+        });
+        socket.on('comment', prependMessage);
+        socket.on('connection', function (data) {
+            $('#connection').text(data + '人');
+        });
+        $('#comments').submit(function (e) {
+            e.preventDefault();
+            var input = $('#message');
+            var text = input.val();
+            if (text.length > 0) {
+                if (text.length < 50) {
+                    socket.emit('comment', input.val());
+                    prependMessage({
+                        date: new Date().getTime(),
+                        name: myname,
+                        text: text
+                    });
+                }
+                input.val('');
+            }
+        });
+        $('#message').focus();
     });
-    $('#message').focus();
-    // anywhere
-    twttr.anywhere(function (T) {
-        T('#footer').linkifyUsers({ className: 'blank' });
-        $('a.blank').attr({ target: '_blank' });
-    });
-});
+}
