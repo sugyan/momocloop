5d1d89339bd4f16046f75a1f6807792e2ddbf7c8
sugyan <sugi1982@gmail.com>
2011-09-21 22:37:07 +0900

    use environment variable "mongo_url"

--- config/example.sh
+++ config/example.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 npm config set momocloop:_http_session_secret **********************
-npm config set momocloop:_mongodb_host *****************
-npm config set momocloop:_mongodb_port *****
-npm config set momocloop:_mongodb_dbname *********
-npm config set momocloop:_mongodb_username ******
-npm config set momocloop:_mongodb_password ********
 npm config set momocloop:_twitter_consumer_key **********************
 npm config set momocloop:_twitter_consumer_secret ***************************************
 npm config set momocloop:_ustream_key ********************************
+npm config set momocloop:_mongo_url 'mongodb://****:****@****:****/****'
+npm config set momocloop:_redis_url 'redis://****:****@****:****/'
