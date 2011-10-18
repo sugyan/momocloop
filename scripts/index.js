0610692207cfcdf7f5f4943d89b6173ed59eeb5c
sugyan <sugi1982@gmail.com>
2011-10-18 11:52:48 +0900

    update script

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 var url = require('url');
-var async = require('async');
 var mongodb = require('mongodb');
 var model = require('./../lib/model');
 
@@ -18,35 +17,14 @@ var db = (function () {
     return new model.Db(config);
 }());
 
-function ensureIndex () {
-    async.series([
-        function (callback) {
-            db.collection('comment', function (err, collection) {
-                if (err) { callback(err); }
-                collection.ensureIndex({ date: -1, room: 1 }, function (er, results) {
-                    if (err) { callback(err); }
-                    console.log(results);
-                    callback();
-                });
-            });
-        },
-        function (callback) {
-            db.collection('program', function (err, collection) {
-                if (err) { callback(err); }
-                collection.ensureIndex({ type: 1, finished: 1 }, function (er, results) {
-                    if (err) { callback(err); }
-                    console.log(results);
-                    callback();
-                });
-            });
-        }
-    ], function (err, results) {
-        if (err) { throw err; }
-        db.close();
-    });
-}
-
 db.open(function (err) {
     if (err) { throw err; }
-    ensureIndex();
+    db.collection('comment', function (err, collection) {
+        if (err) { callback(err); }
+        collection.ensureIndex({ date: -1, room: 1 }, function (er, results) {
+            if (err) { callback(err); }
+            console.log(results);
+            db.close();
+        });
+    });
 });
