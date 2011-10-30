aa20aa121b595f72b75483dad7152418f15b0f37
sugyan <sugi1982@gmail.com>
2011-10-31 01:21:24 +0900

    add log

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 require('./lib/socket.io').listen(app);
+console.log('started.');
