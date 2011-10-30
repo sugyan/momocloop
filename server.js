37a9fd1e2021769c30ec953e0cfc9c816f1c849d
sugyan <sugi1982@gmail.com>
2011-10-31 01:31:39 +0900

    Revert "add log"
    This reverts commit aa20aa121b595f72b75483dad7152418f15b0f37.

--- server.js
+++ server.js
@@ -4,4 +4,3 @@ app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 require('./lib/socket.io').listen(app);
-console.log('started.');
